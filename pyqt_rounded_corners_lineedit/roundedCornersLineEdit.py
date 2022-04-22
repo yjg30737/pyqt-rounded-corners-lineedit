bddb449f38191f54b5d87a93e3129ed8cc0fb4c4
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-22 09:08:00 +0900

    Refactoring, catch font change event and apply it to lineedit style properly

--- pyqt_rounded_corners_lineedit/roundedCornersLineEdit.py
+++ pyqt_rounded_corners_lineedit/roundedCornersLineEdit.py
@@ -5,9 +5,18 @@ from python_get_absolute_resource_path.getAbsoulteResourcePath import get_absolu
 class RoundedCornersLineEdit(QLineEdit):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.__initStyle()
+
+    def __initStyle(self):
         css_file = open(get_absolute_resource_path('style/lineedit.css'))
         css_code = css_file.read()
         css_file.close()
-        border_radius = self.sizeHint().height()/2.5
-        padding = self.sizeHint().height()/7
-        self.setStyleSheet(css_code.format(border_radius, padding))
+        border_radius = self.sizeHint().height()/2
+        padding = self.sizeHint().height()/5
+        self.setStyleSheet(self.styleSheet() + css_code.format(border_radius, padding))
+
+    def event(self, e):
+        # catch font change event
+        if e.type() == 97:
+            self.__initStyle()
+        return super().event(e)
